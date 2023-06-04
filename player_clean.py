b7bf1fdf94cc65b9939b16cb8f281d69257469ae
Nathan Wright <nathan.r.wright11@gmail.com>
2023-06-03 21:34:36 -0400

    Cleans data & adds labels to assist in analysis

--- player_clean.py
+++ player_clean.py
@@ -1,15 +1,12 @@
 import pandas as pd
 
-players = ['stephen curry',
-           'lebron james',
-           'jayson tatum',
-           'jimmy butler', 
-           'nikola jokic',
-           'kevin durant',
-           'joel embiid',
-           'giannis antetokounmpo']
-
 def clean_games(file):
+    """Removes unnecessary rows & columns, adds regular/playoff label as column.
+    
+    Removes columns with no useable data. Removes games which player was not active
+    (eg. Inactive, DND, DNP, etc.). Adds label for each game whether it was regular 
+    season or playoffs.
+    """
     df = pd.read_csv(file)
     df = df.drop(columns=[' ', ' .1'])
     df = df.dropna(subset='MP')
@@ -18,18 +15,33 @@ def clean_games(file):
     return df
 
 def date_sort(df):
+    """Sorts games in order by date, then resets the df indexes"""
     df['Date'] = pd.to_datetime(df['Date'])
     df = df.sort_values('Date', ascending=True)
     df = df.reset_index(drop=True)
     return df
 
 def s_label(date):
+    """Creates season format for use as data label.
+    
+    Takes date input (eg. 2010-01-28) and returns output which represents the season 
+    that game occured in (eg. 09-10).
+    """
     yr, mon, day = date.split("-")
     if int(mon) >= 8:
         return f"{yr[-2:]}-{int(yr[-2:])+1}"
     else:
         return f"{int(yr[-2:])-1:02d}-{yr[-2:]}"
 
+
+players = ['stephen curry',
+           'lebron james',
+           'jayson tatum',
+           'jimmy butler', 
+           'nikola jokic',
+           'kevin durant',
+           'joel embiid',
+           'giannis antetokounmpo']
 
 for player in players:
     first, last = player.split()
